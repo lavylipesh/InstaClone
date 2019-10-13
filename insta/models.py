c88b2380e85d1473223dfeb9c7711f7db58f8570
lavylipesh <lilibethlangat@yahoo.com>
2019-10-13 22:38:41 +0300

    create defaault for image

--- insta/models.py
+++ insta/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+import datetime as dt
 
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='static/', blank = 'true')
     bio = models.TextField(default="")
 
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'images/')
+  
     image_name = models.CharField(max_length = 60)
     image_caption = models.CharField(max_length=60)
     profile = models.ForeignKey(Profile)
     pub_date = models.DateTimeField(auto_now_add=True)
+      image = models.ImageField(upload_to = 'images/')
     
    
    
