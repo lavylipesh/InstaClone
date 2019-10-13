25dbd985c795b0efdcebcb34fe15e7650ed353e0
lavylipesh <lilibethlangat@yahoo.com>
2019-10-14 01:35:09 +0300

    perfecting serch_term

--- insta/views.py
+++ insta/views.py
@@ -5,12 +5,12 @@ def index(request):
     return render(request,'index.html')
 
 def search_results(request):
-    if  'name' in request.GET and request.GET["name"]:
-        search_term = request.GET.get("name")
-        searched_names = bio.search_by_name(search_term)
+    if  'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_name(search_term)
         message = f"{search_term}"
 
-        return render(request,'search.html',{"message":message,"names":searched_names})
+        return render(request,'search.html',{"message":message,"profiles":searched_profiles})
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
